9cc438c7aecc09ef8ccc61b123ca677e4d91a17f
RenneSA <66268373+RenneSA@users.noreply.github.com>
2020-08-24 11:47:59 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myarray = [10,'Daciuk','null',{a:1},function() {}];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myfunction(arr){
+    return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myfunction(myarray)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFunction2(arr,index){
+    return arr[index];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+ var myarra2 = [ninja, 52.4, true,[1,2,'Daciuk'],{b:2}]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(myfunction2(myarra2,0));
+console.log(myfunction2(myarra2,1));
+console.log(myfunction2(myarra2,2));
+console.log(myfunction2(myarra2,3));
+console.log(myfunction2(myarra2,4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,54 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName){
+    var allBooks = {
+        'Segredos Do Ninja JavaScript':{
+            quantidadePaginas: 488,
+            autor:'John Resing & bear bibeault',
+            editora:'Novatec'
+        },
+        'Introdução ao HTML5':{
+            quantidadePaginas:220,
+            autor:'Bruce Lawson & Remy Sharp',
+            editora:'Alta Books'
+        },
+        'Smashig CSS':{
+            quantidadePaginas: 283,
+            autor:'Erick a. Meyer',
+            editora:'Bookman'
+        }
+    };
+    
+
+    return !bookName ? allBooks : allBooks [bookName];
+}
+
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro Introdução ao HTML5 tem '+book('Introdução ao HTML5').quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+var bookName = 'Smashig CSS';
+console.log('O Autor do livro'+ bookName + ' é '+book(bookName).autor + ' .');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+var bookName = 'Smashig CSS';
+console.log('O livro '+ bookName + ' foi publicado pela editora '+book(bookName).editora + ' .');
